a6a8c036f26940b726bb53f2475fe910ba7b3c52
OpenShift guest <brandon.luders@gmail.com>
2013-09-17 15:19:51 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp2::Application.config.secret_token = '40754dc2de3949bf85bcee41347a4f081fd2cf0ba47e122c688c73d2062ae9d54e8be5169040b58e301b26cc416174456572b8613cd101d020322491f1380586'
